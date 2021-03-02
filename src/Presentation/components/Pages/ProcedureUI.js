cac19ee6837b0e8b57ce447f836f7cb3b876ed05
NatsukiH <natsu.0l7e2o7.s2@gmail.com>
2021-03-02 22:51:48 +0900

    ProcedureUIでStepperの呼び出し，Stepperで各手順の呼び出し

--- src/Presentation/components/Pages/ProcedureUI.js
+++ src/Presentation/components/Pages/ProcedureUI.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import DefaultLayout from '../templates/DefaultLayout';
-import Step1Keyword from '../Procedures/Step1Keyword';
-import Step2Problem from '../Procedures/Step2Problem';
-import Step3Solution from '../Procedures/Step3Solution';
-import Step4Vote from '../Procedures/Step4Vote';
+import Stepper from '../Steppers/Stepper';
 
 import '../../../App.css';
 
@@ -11,6 +7,7 @@ function ProcedureUI() {
   return (
     <DefaultLayout>
         <h1>ProcedureUI</h1>
+        <Stepper />
     </DefaultLayout>
   );
 }
--- src/Presentation/components/Steppers/Stepper.js
+++ src/Presentation/components/Steppers/Stepper.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Step1Keyword from '../Procedures/Step1Keyword';
+import Step2Problem from '../Procedures/Step2Problem';
+import Step3Solution from '../Procedures/Step3Solution';
+import Step4Vote from '../Procedures/Step4Vote';
 
-function Step3Solution() {
+
+function Stepper() {
   return (
-    <p>Step3Solution</p>
+    <p>steppr</p>
   );
 }
 
-export default Step3Solution;
+export default Stepper;
