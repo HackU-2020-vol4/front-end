a3057e364c3cbe15bdf16e5c2e791d1a13ef400f
fuki <fuki01@users.noreply.github.com>
2021-03-09 10:40:13 +0900

    add: homeとのAPI接続

--- src/Presentation/components/Pages/Home.js
+++ src/Presentation/components/Pages/Home.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from 'react-router-dom';
 import DefaultLayout from '../templates/DefaultLayout';
 import {
   Box,
@@ -9,8 +9,9 @@ import {
   TextField
 } from '@material-ui/core';
 import '../../../App.css';
+import axios from "axios";
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
   get_button: {
     marginTop: 45,
     marginRight: 30
@@ -24,14 +25,45 @@ const useStyles = makeStyles(theme => ({
     marginTop: 45,
     marginLeft: 30
   }
-}));
+});
 
-function Home() {
-  const classes = useStyles();
-  const [room_id, setValue] = useState('default');
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {
+      rooms: [],
+      roomid: '',
+    };
+  }
+  handleClick(){
+    this.props.history.push({
+      pathname:'/procedure',
+      state: {roomid: this.state.roomid}
+    })
+  }
+  handleChange(e){
+    this.setState({
+      roomid: e.target.value
+    })
+    console.log(e.target.value)
+  }
+
+createRoomid = (response) => {
+  axios.post(`http://localhost:8080/publishers/create`)
+  .then(results => {
+    console.log(results.data);
+    this.setState({ rooms: results.data });
+  })
+}
+
+render() {
+  const { classes } = this.props;
   return (
     <div className="App">
       <DefaultLayout>
+      { this.state.rooms.RoomID }      
         <Typography variant="h4" style={{margin: "3vh auto"}}>What's Hackathon Supporter?</Typography>
         <Typography variant="body1" gutterBottom>
           ハッカソンで何かを作りたいが何を作ればいいかわからない。。。
@@ -45,18 +77,20 @@ function Home() {
           <div style={{ whiteSpace: "pre-line" }}></div>
           対面はもちろん、オンライン通話をしながら使用することを想定しています。
         </Typography>
-        <Button className={classes.get_button} onClick={()=>alert('Room IDが発行されました（されてない）')} variant="contained">Get Room ID</Button>
+        <Button className={classes.get_button} onClick={() => this.createRoomid()} variant="contained">Get Room ID</Button>
         <TextField
-          onChange={(event) => setValue(event.target.room_id)}
+          value={this.state.roomid} 
+          onChange={this.handleChange}
           className ={classes.textfield}
           variant="filled"
           label="Room ID"
           color="secondary.light "
         />
-        <Button className={classes.start_button} href="procedure"  variant="contained" disabled={room_id === "default"}>Start</Button>
+        <Button className={classes.start_button} onClick={this.handleClick} variant="contained" disabled={this.state.roomid === ''}>Enter Room</Button>
       </DefaultLayout>
     </div>
   );
 }
+}
 
-export default Home;
+export default withRouter(withStyles(styles, { withTheme: true })(Home));
