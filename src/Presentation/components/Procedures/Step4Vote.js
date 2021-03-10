855668236760bcbf833d72573cf234bf96db5b07
yusei0329 <nwys.28@gmail.com>
2021-03-11 01:03:26 +0900

    downボタンの追加

--- src/Presentation/components/Procedures/Step4Vote.js
+++ src/Presentation/components/Procedures/Step4Vote.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Count from '../coutParts/count';
 
+
 class Step4Vote extends React.Component {
 
     render() {
-        const solutionList= ["解決策1", "解決策2", "解決策3"]
-        
-        return(
+        const solutionList = ["解決策1", "解決策2", "解決策3"]
+        return (
             <div>
-            {solutionList.map((element) => (
-                <div>
-                    <p>{element}</p>
-                <Count  />
-                </div>
-            ))}
+                {solutionList.map((element) => (
+                    <div>
+                        <p>{element}</p>
+                        <Count />
+                    </div>
+                ))}
             </div>
         )
     }
--- src/Presentation/components/coutParts/count.js
+++ src/Presentation/components/coutParts/count.js
@@ -1,30 +1,63 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Count extends Component {
 
     constructor() {
         super();
         this.state = {
-          count: 0,
-    };
+            count: 0,
+            roomid: "test",
+        };
+    }
+    
+
+    countUp() {
+        var n = Number(localStorage.getItem(this.state.roomid));
+        if(localStorage.getItem('test') == null) {
+            if(n !== 0){
+                n=0;
+        }
+    }
+        if(n >= 3){
+            localStorage.setItem(this.state.roomid, n = 3);
+            this.setState({
+                count: this.state.count
+            });
+        }else{
+            localStorage.setItem(this.state.roomid, n += 1);
+            this.setState({
+                count: this.state.count + 1
+            });
+        }
+        console.log(n);
+    }
+    countDown() {
+        var n = Number(localStorage.getItem(this.state.roomid));
+        if (n <= 0) {
+            localStorage.setItem(this.state.roomid, n = 0);
+            this.setState({
+                count: 0
+            });
+        } else {
+            localStorage.setItem(this.state.roomid, n -= 1);
+            this.setState({
+                count: this.state.count - 1
+            });
+        }
+        console.log(n);
     }
 
-    countUp(){
-    this.setState({
-        count: this.state.count + 1
-    });
-    }
-
-    render(){
+    render() {
         const { count } = this.state;
         return (
             <div>
-                <button onClick={()=>this.countUp()}>ここをクリック</button>
+                <button onClick={() => this.countUp()}>ここをクリック</button>
+                <button onClick={() => this.countDown()}>ここをクリック</button>
                 <p>{count}</p>
             </div>
         );
     }
-    
+
 };
 
 export default Count;
